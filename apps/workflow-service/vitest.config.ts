284caec9afe96ad7a5c6539b030b7d7d53462e7a
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-08-07 23:41:12 +0530

    chore: fix tsconfig.json file with shared ts config, add vitest config

---
+++ apps/workflow-service/vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+import { baseTest } from "../../vitest.config.base";
+
+export default defineConfig({
+  test: {
+    ...baseTest,
+    name: "api:unit",
+    include: ["src/**/*.test.ts"],
+  },
+});
